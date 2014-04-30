7b22cb213df20e6fce1deb4c877999b78be334d3
André Peil <andreguipeil@gmail.com>
2014-04-30 12:22:27 -0300

    Subindo as telas, utilizando modo cardLayout para

--- sistac/src/Principal.java
+++ sistac/src/Principal.java
@@ -1,36 +1,29 @@
 
 
+import system.*;
+import config.*;
 
 
-import system.Atividade;
-import system.Atividade;
-import system.TipoAtividade;
-import system.TipoAtividade;
-
-/**
- * Arquivo para testes iniciais com as classes Atividade, Categoria e TipoAtividade
- * @author andref
- */
 public class Principal {
 
-    /**
-     * Shift+F6 (no netbeans) executa o arquivo aberto, isso ira executar essa classe provisoria ao inves
-     * do projeto principal
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        //carrega os tipos de atividades no sistema
-        TipoAtividade.loadTipoAtividades();
-        //carrega TipoAtividade em um objeto para facilir o uso
-        TipoAtividade monitoria = TipoAtividade.getTipoAtividade("Monitoria");
-        //cria um objeto atividade
-        Atividade atividade = new Atividade("Monitoria de P1", monitoria);
-        //dados recuperados dos objetos
-        System.out.println("Descricao: " + atividade.getDescricao());
-        System.out.println("TipoAtividade: " + atividade.getTipoAtividade().getDescricao());
-        System.out.println("Unidade: " + atividade.getUnidadeAtividade() +" " + atividade.getTipoAtividade().getUnidadeTipoAtividade());
-        System.out.println("Atividade Categoria: " + atividade.getTipoAtividade().getCategoria().getNome());
-        
+    private Config config;
+    private Janela telas;
+   
+    public Principal(){
+        this.config = Config.getInstancia();
+        this.telas = new Janela();
     }
     
+    
+    public static void main(String[] args) {
+        
+        Principal sistac = new Principal();
+        sistac.comecar();
+
+    }
+    
+    public void comecar(){
+    
+       this.telas.setVisible(true);
+    }
 }
